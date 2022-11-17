c1b814e5d342f7c96e614ddcd69f827900677036
SchizoBoy <cadonienzo@gmail.com>
2022-11-17 22:11:34 +0100

    feat : réduction du main

--- machines_virtuelles/partage/tunnel64d/src/main.c
+++ machines_virtuelles/partage/tunnel64d/src/main.c
@@ -2,30 +2,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "iftun.h"
+#include "parallele.h"
 
 int main(int argc, char **argv){
-  int tunfd;
-
-  printf("Création de %s\n", argv[1]);
-
-  tunfd = tun_alloc(argv[1]);
-
-  printf("Faire la configuration de %s...\n", argv[1]);
-
-  printf("Appuyez sur une touche pour continuer\n");
-
-  getchar();
-
-  printf("Interface %s Configurée:\n",argv[1]);
-
-  system("ip addr");
-
-  printf("Transfert initié sur %s\n", argv[1]);
-
-  printf("%d\n", tunfd);
-
-  transfert(tunfd, 1);
-
+  demarrer_tunnel(argv[1], argv[2], argv[3]);
   exit(0);
 }
